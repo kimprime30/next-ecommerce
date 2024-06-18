44da12d769f6933c228ff245255a994ee3ce34c0
francisco aquino <kino.666@gmail.com>
2024-06-18 10:39:56 -0300

    inicio do bacend com wix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,31 @@
+//"use client";
+
 import CategoryList from "@/components/CategoryList";
 import ProducList from "@/components/ProductList";
 import Slider from "@/components/Slider";
+import { WixClientContext } from "@/context/wixContext";
+import { useWixClient } from "@/hooks/useWixClient";
+import { wixClientServer } from "@/lib/wixClientServer";
+import { useContext, useEffect } from "react";
 
-const HomePage = () => {
+const HomePage = async () => {
+  // const wixClient = useWixClient();
+
+  // useEffect(() => {
+  //   const getProducts = async () => {
+  //     const res = await wixClient.products.queryProducts().find();
+
+  //     console.log(res);
+  //   };
+
+  //   getProducts();
+  // }, [wixClient]);
+
+  const wixClient = await wixClientServer();
+  const res = await wixClient.products.queryProducts().find();
+
+  console.log(res);
+
   return (
     <div className="">
       <Slider />
