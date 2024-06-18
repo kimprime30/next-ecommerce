acef9537084cd2baf790343a42952d1b4e6e7b04
francisco aquino <kino.666@gmail.com>
2024-06-18 08:35:59 -0300

    frontend quase completo

---
+++ src/components/ProductImages.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+const images = [
+  {
+    id: 1,
+    url: "https://images.pexels.com/photos/20780453/pexels-photo-20780453/free-photo-of-natureza-filiais-setores-galhos.jpeg?auto=compress&cs=tinysrgb&w=400",
+  },
+  {
+    id: 2,
+    url: "https://images.pexels.com/photos/20780442/pexels-photo-20780442/free-photo-of-natureza-filiais-setores-galhos.jpeg?auto=compress&cs=tinysrgb&w=400",
+  },
+  {
+    id: 3,
+    url: "https://images.pexels.com/photos/20793353/pexels-photo-20793353/free-photo-of-madeira-agua-flores-arbusto.jpeg?auto=compress&cs=tinysrgb&w=400",
+  },
+  {
+    id: 4,
+    url: "https://images.pexels.com/photos/20749268/pexels-photo-20749268/free-photo-of-natureza-filiais-setores-galhos.jpeg?auto=compress&cs=tinysrgb&w=400",
+  },
+];
+
+const ProductImages = () => {
+  const [index, setIndex] = useState(0);
+
+  return (
+    <div className="">
+      <div className="h-[500px] relative">
+        <Image
+          src={images[index].url}
+          alt=""
+          fill
+          sizes="50vw"
+          className="object-cover rounded-md"
+        />
+      </div>
+      <div className="flex justify-between gap-4 mt-8">
+        {images.map((images, i) => (
+          <div
+            className="w-1/4 h-32 relative gap-4 mt-8 cursor-pointer  "
+            key={images.id}
+            onClick={() => setIndex(i)}
+          >
+            <Image
+              src={images.url}
+              alt=""
+              fill
+              sizes="30vw"
+              className="object-cover rounded-md"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductImages;
