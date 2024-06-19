3375c7b5a2facc775c6dd0625710d6e1e76ffbd4
francisco aquino <kino.666@gmail.com>
2024-06-18 22:17:18 -0300

    metade do backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Slider from "@/components/Slider";
 import { WixClientContext } from "@/context/wixContext";
 import { useWixClient } from "@/hooks/useWixClient";
 import { wixClientServer } from "@/lib/wixClientServer";
-import { useContext, useEffect } from "react";
+import { Suspense, useContext, useEffect } from "react";
 
 const HomePage = async () => {
   // const wixClient = useWixClient();
@@ -21,17 +21,22 @@ const HomePage = async () => {
   //   getProducts();
   // }, [wixClient]);
 
-  const wixClient = await wixClientServer();
-  const res = await wixClient.products.queryProducts().find();
+  // const wixClient = await wixClientServer();
+  // const res = await wixClient.products.queryProducts().find();
 
-  console.log(res);
+  // console.log(res);
 
   return (
     <div className="">
       <Slider />
       <div className="mt-24 px-4 md:px-8 lg:px-16 cl:px-32 2xl:px-64">
         <h1 className="text-2xl">Featured Products</h1>
-        <ProducList />
+        <Suspense fallback={"loading"}>
+          <ProducList
+            categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!}
+            limit={4}
+          />
+        </Suspense>
       </div>
       <div className="mt-24 ">
         <h1 className="text-2xl px-4 md:px-8 lg:px-16 cl:px-32 2xl:px-64 mb-12">
@@ -41,7 +46,6 @@ const HomePage = async () => {
       </div>
       <div className="mt-24 px-4 md:px-8 lg:px-16 cl:px-32 2xl:px-64">
         <h1 className="text-2xl">New Products</h1>
-        <ProducList />
       </div>
     </div>
   );
