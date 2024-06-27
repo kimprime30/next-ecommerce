8d893ca1a679b6bbbed15a0a55b4306a080ad092
francisco aquino <kino.666@gmail.com>
2024-06-26 22:14:07 -0300

    produtos com a descriçao do wix

--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -3,33 +3,33 @@
 import Image from "next/image";
 import { useState } from "react";
 
-const images = [
-  {
-    id: 1,
-    url: "https://images.pexels.com/photos/20780453/pexels-photo-20780453/free-photo-of-natureza-filiais-setores-galhos.jpeg?auto=compress&cs=tinysrgb&w=400",
-  },
-  {
-    id: 2,
-    url: "https://images.pexels.com/photos/20780442/pexels-photo-20780442/free-photo-of-natureza-filiais-setores-galhos.jpeg?auto=compress&cs=tinysrgb&w=400",
-  },
-  {
-    id: 3,
-    url: "https://images.pexels.com/photos/20793353/pexels-photo-20793353/free-photo-of-madeira-agua-flores-arbusto.jpeg?auto=compress&cs=tinysrgb&w=400",
-  },
-  {
-    id: 4,
-    url: "https://images.pexels.com/photos/20749268/pexels-photo-20749268/free-photo-of-natureza-filiais-setores-galhos.jpeg?auto=compress&cs=tinysrgb&w=400",
-  },
-];
+// const images = [
+//   {
+//     id: 1,
+//     url: "https://images.pexels.com/photos/20780453/pexels-photo-20780453/free-photo-of-natureza-filiais-setores-galhos.jpeg?auto=compress&cs=tinysrgb&w=400",
+//   },
+//   {
+//     id: 2,
+//     url: "https://images.pexels.com/photos/20780442/pexels-photo-20780442/free-photo-of-natureza-filiais-setores-galhos.jpeg?auto=compress&cs=tinysrgb&w=400",
+//   },
+//   {
+//     id: 3,
+//     url: "https://images.pexels.com/photos/20793353/pexels-photo-20793353/free-photo-of-madeira-agua-flores-arbusto.jpeg?auto=compress&cs=tinysrgb&w=400",
+//   },
+//   {
+//     id: 4,
+//     url: "https://images.pexels.com/photos/20749268/pexels-photo-20749268/free-photo-of-natureza-filiais-setores-galhos.jpeg?auto=compress&cs=tinysrgb&w=400",
+//   },
+// ];
 
-const ProductImages = () => {
+const ProductImages = ({ items }: { items: any }) => {
   const [index, setIndex] = useState(0);
 
   return (
     <div className="">
       <div className="h-[500px] relative">
         <Image
-          src={images[index].url}
+          src={items[index].image?.url}
           alt=""
           fill
           sizes="50vw"
@@ -37,14 +37,14 @@ const ProductImages = () => {
         />
       </div>
       <div className="flex justify-between gap-4 mt-8">
-        {images.map((images, i) => (
+        {items.map((item: any, i: number) => (
           <div
             className="w-1/4 h-32 relative gap-4 mt-8 cursor-pointer  "
-            key={images.id}
+            key={item._id}
             onClick={() => setIndex(i)}
           >
             <Image
-              src={images.url}
+              src={item.image?.url}
               alt=""
               fill
               sizes="30vw"
