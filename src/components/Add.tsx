bae69c71b858d6a72d71d8d94d90abb9d798e74b
francisco aquino <kino.666@gmail.com>
2024-07-02 19:48:24 -0300

    select size and color and quantity

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -2,17 +2,25 @@
 
 import { useState } from "react";
 
-const Add = () => {
+const Add = ({
+  productId,
+  variantId,
+  stockNumber,
+}: {
+  productId: string;
+  variantId: string;
+  stockNumber: number;
+}) => {
   const [quantity, setQuantity] = useState(1);
 
-  //TEMPORARY
-  const stock = 4;
+  // //TEMPORARY
+  // const stock = 4;
 
   const handleQuantity = (type: "i" | "d") => {
     if (type === "d" && quantity > 1) {
       setQuantity((prev) => prev - 1);
     }
-    if (type === "i" && quantity < stock) {
+    if (type === "i" && quantity < stockNumber) {
       setQuantity((prev) => prev + 1);
     }
   };
@@ -37,7 +45,8 @@ const Add = () => {
             </button>
           </div>
           <div className="text-xs">
-            Only <span className="text-orange-500">4 items</span> left! <br />
+            Only <span className="text-orange-500">{stockNumber} items</span>{" "}
+            left! <br />
             {"Don't"} miss it
           </div>
         </div>
